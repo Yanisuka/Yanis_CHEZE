0e9b7558ca9bad64c30335bda42413835d0c5855
Yanisuka <78016644+Yanisuka@users.noreply.github.com>
2021-02-01 16:53:18 +0100

    Add files via upload

---
+++ Yanis CHEZE TP JS/exercice3.js
@@ -0,0 +1,19 @@
+
+
+function fonction1(){
+
+var chaine = "Ceci est une chaine de caractere d'une fonction js" ; 
+
+document.write("Valeur chaine : " +chaine+" <br>");
+
+var nb1 = 5;
+var nb2 = 9;
+var nb3 = 17;
+var nb4 = 28;
+
+
+document.write("<table width="+100+" border="+1+"><tr><td>"+nb1+"</td><td>"+nb2+"</td></tr><tr><td>"+nb3+"</td><td>"+nb4+"</td></tr></table>")
+
+}
+
+fonction1();
